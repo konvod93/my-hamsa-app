8a405a2fc34798e4bcf869568cd7d87cdf25f6fb
Konstantin <konvod93@gmail.com>
2025-08-11 10:43:53 +0300

    Added Header and Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 text-center">It is a Footer</footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full bg-blue-500 text-white p-4">It is a Header</header>
+  );
+}
+
+export default Header;
