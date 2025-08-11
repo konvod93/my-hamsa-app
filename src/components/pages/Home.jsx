fafa4e6c54f498ed94cce9ba1899d4278b9c9c3e
Konstantin <konvod93@gmail.com>
2025-08-11 14:35:26 +0300

    Added Processs component to Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Hero";
 import Features from "../Features";
+import Process from "../Process";
 
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen text-black">
         <Hero />
         <Features />
+        <Process />
     </div>
   );
 }
