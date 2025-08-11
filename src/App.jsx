373d7bb9a2c69eb9c227082c87281c0d505cdb10
Konstantin <konvod93@gmail.com>
2025-08-11 12:51:17 +0300

    updated Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 pt-24 pb-36 overflow-auto">
+      <main className="flex-1 pt-16 pb-36 overflow-auto">
         <Routes>          
           <Route path="/" element={<Home />} />
           {/* Add more routes as needed */}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-gray-800">Welcome to My Hamsa App</h1>
-        <p className="mt-4 text-lg text-gray-600">This is a simple React application with Tailwind CSS.</p>
-        <Link to="/" className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Go to Home</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-violet-950">
+        <img src="/hamsa.png" alt="Hamsa" className="w-48 h-48 mb-4" />
+        <h1 className="text-4xl font-bold text-amber-200">Welcome to out Magical Service</h1>
+        <p className="mt-4 text-lg text-amber-200">Creater your own individual talisman helping in your life</p>
+        <Link to="/" className="mt-6 px-4 py-2 bg-amber-500 text-white rounded hover:bg-amber-300">Create!</Link>
     </div>
   );
 }
